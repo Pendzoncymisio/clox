1c81ed0ae6ae359f0ef75e56cc000748307b2e00
Pendzoncymisio <pendzoncymisio@gmail.com>
2023-02-15 12:40:04 -0500

    dynamic typing and strings

--- debug.c
+++ debug.c
@@ -21,6 +21,9 @@ static int constantInstruction(const char* name, Chunk* chunk, int offset) {
 
 static int simpleInstruction(const char* name, int offset) {
     printf("%s", name);
+    int count;
+    for (count = 0; name[count] != '\0'; ++count);
+    printf("%*s", 25 - count, "");
     return offset + 1;
 }
 
@@ -37,10 +40,17 @@ int disassembleInstruction(Chunk* chunk, int offset) {
     switch (instruction)
     {
     case OP_CONSTANT: return constantInstruction("OP_CONSTANT", chunk, offset);
+    case OP_NIL: return simpleInstruction("OP_NIL", offset);
+    case OP_TRUE: return simpleInstruction("OP_TRUE", offset);
+    case OP_FALSE: return simpleInstruction("OP_FALSE", offset);
+    case OP_EQUAL: return simpleInstruction("OP_EQUAL", offset);
+    case OP_GREATER: return simpleInstruction("OP_GREATER", offset);
+    case OP_LESS: return simpleInstruction("OP_LESS", offset);
     case OP_ADD: return simpleInstruction("OP_ADD", offset);
     case OP_SUBTRACT: return simpleInstruction("OP_SUBTRACT", offset);
     case OP_MULTIPLY: return simpleInstruction("OP_MULTIPLY", offset);
     case OP_DIVIDE: return simpleInstruction("OP_DIVIDE", offset);
+    case OP_NOT: return simpleInstruction("OP_NOT", offset);
     case OP_NEGATE: return simpleInstruction("OP_NEGATE", offset);
     case OP_RETURN: return simpleInstruction("OP_RETURN", offset); 
     default:
