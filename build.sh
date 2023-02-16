997c62e2b06101f1773c9c9ec19211abc1f7baa6
Pendzoncymisio <pendzoncymisio@gmail.com>
2023-02-16 16:04:43 -0500

    Hash tables

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-gcc -o clox main.c chunk.c memory.c debug.c value.c vm.c compiler.c scanner.c object.c
+gcc -o clox main.c chunk.c memory.c debug.c value.c vm.c compiler.c scanner.c object.c table.c
 ./clox
