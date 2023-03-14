ed320ffc623b567859e4a2afbcb286d68b227b79
Pendzoncymisio <pendzoncymisio@gmail.com>
2023-03-14 16:13:39 -0400

    Optional debugger build

--- build.sh
+++ build.sh
@@ -1,1 +1,17 @@
-gcc -g -o clox src/main.c src/chunk.c src/memory.c src/debug.c src/value.c src/vm.c src/compiler.c src/scanner.c src/object.c src/table.c
+#!/bin/bash
+ 
+while getopts "g" opt; do
+  case $opt in
+    g)
+      gcc -g -o clox src/main.c src/chunk.c src/memory.c src/debug.c src/value.c src/vm.c src/compiler.c src/scanner.c src/object.c src/table.c
+      echo "CLox debug build successful"
+      exit
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+gcc -o clox src/main.c src/chunk.c src/memory.c src/debug.c src/value.c src/vm.c src/compiler.c src/scanner.c src/object.c src/table.c
+echo "CLox build successful"
